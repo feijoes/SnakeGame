1c38141c1f235b66ad0e699333de2233e70db3d6
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-08-07 16:09:23 -0300

    add start game page

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,30 +1,43 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 
 public class MyFrame extends JFrame {
     JButton button;
     JLabel label;
+    JPanel panel2;
+    JPanel panel1 ;
     public MyFrame(){
         this.setTitle("Snake Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
-
-        this.setIconImage( new ImageIcon("logo.png").getImage());;
+        this.getContentPane().setLayout(new GridLayout(2,2));
+        this.setIconImage( new ImageIcon("logo.png").getImage());
         this.getContentPane().setBackground(Color.lightGray);
+
+        panel2 = new JPanel();
+        panel1 = new JPanel();
 
         button = new JButton();
         button.setFocusPainted(false);
         button.setFocusable(false);
         button.addActionListener(e -> System.out.println("Pressed"));
         button.setText("Start Game");
-        button.setIcon(new ImageIcon(new ImageIcon("start.png").getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT)));
-        this.add(button);
+        button.setIcon(new ImageIcon(new ImageIcon("start.png").getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT)));
+        panel2.add(button);
 
-        label = new JLabel();
-        label.setText("Snake game");
-        this.add(label);
+        label = new JLabel("Snake Game",  SwingConstants.CENTER);
+        label.setFont(new Font(label.getFont().getName(), label.getFont().getStyle(), 40));
+        label.setPreferredSize(new Dimension(400, 200));
+        label.setLocation(300,700);
+        panel1.add(label);
 
+
+        panel1.setVisible(true);
+        panel2.setVisible(true);
+        this.add(panel1);
+        this.add(panel2);
         this.setVisible(true);
     }
 
