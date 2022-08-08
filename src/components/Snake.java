c1557e724d479b2aa2ea50c5e75869b74c3c541f
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-08-07 23:15:55 -0300

    add square and snake

--- src/components/Snake.java
+++ src/components/Snake.java
@@ -11,13 +11,13 @@ public class Snake {
     private Timer timer;
     Snake(){
         timer = new Timer(500, e -> {
-            for (int i=x;i>=x-3;i--){
+            for (int i=y;i>=y-3;i--){
                 JLabel newsquare = new JLabel();
                 newsquare.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
                 newsquare.setBackground(Color.red);
                 newsquare.setOpaque(true);
                 newsquare.setVisible(true);
-                Squares.squares[i][y] = newsquare;
+                Squares.squares[x][i] = newsquare;
             }
 
 
